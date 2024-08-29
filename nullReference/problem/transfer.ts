64cf433a441b9bfebbd811012e63551fee67afa0
muhammadfajri-tif <149763217+muhammadfajri-tif@users.noreply.github.com>
2024-08-29 17:05:48 +0700

    fix(nullReference): add flag for wrong code

--- nullReference/problem/transfer.ts
+++ nullReference/problem/transfer.ts
@@ -9,14 +9,18 @@ export function transferFunds(senderAccountNumber: string, receiverAccountNumber
   const senderAccount = findAccountByNumber(senderAccountNumber);
   const receiverAccount = findAccountByNumber(receiverAccountNumber);
 
-  // Tidak ada pengecekan null
-  if (senderAccount.balance < amount) { // Berpotensi menyebabkan null reference jika senderAccount null
+  // WARN: Tidak ada pengecekan null
+  // Berpotensi menyebabkan null reference jika senderAccount null
+  // TODO: Lakukan pengecekan untuk null terlebih dahulu
+  if (senderAccount.balance < amount) {
     return "Insufficient funds.";
   }
 
-  // Lakukan transfer tanpa pengecekan null
+  // WARN: Lakukan transfer tanpa pengecekan null
+  // TODO: Lakukan pengecekan untuk null terlebih dahulu
   senderAccount.balance -= amount;
-  receiverAccount.balance += amount; // Berpotensi menyebabkan null reference jika receiverAccount null
+  // Berpotensi menyebabkan null reference jika receiverAccount null
+  receiverAccount.balance += amount;
 
   return `Transfer of $${amount} from ${senderAccount.accountHolder} to ${receiverAccount.accountHolder} was successful.`;
 }
